ebae75b260f919e138d427b6c1e8c01da1bf9adc
中島浩二 <knaka0209@gmail.com>
2021-06-14 20:31:23 +0900

    up,0614a3

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
  date    : 2021/06/13
 
- update  :
+ update  : 2021/06/14
 
 ***
 ### Summary
@@ -24,6 +24,10 @@ yarn dev
 
 ***
 ### Blog :
+
+* Apollo server + express + mysql で、GraphQL操作する
+
+https://zenn.dev/knaka0209/articles/634060d4c7e4e4
 
 ***
 
--- src/index.js
+++ src/index.js
@@ -12,6 +12,11 @@ const typeDefs = gql`
     todo(id: Int): Todo
     todos: [Todo]    
   }
+  type Mutation {
+    addTodo(title: String!): Todo
+    updateTodo(id: Int!, title: String!): Todo
+    deleteTodo(id: Int!): Todo
+  }
 `;
 // Provide resolver functions for your schema fields
 
@@ -19,14 +24,29 @@ const resolvers = {
   Query: {
     hello: () => 'Hello world!',
     async todo(parent, args, context, info){
-//      return todos.find(todo => todo.id === args.id);
       return await LibTodos.get_todo(args.id);
     },
     todos:async () => {
-      //return todos
       return await LibTodos.get_items()
     },    
   },
+  Mutation: {
+    addTodo: async (parent, args, context) => {
+      var ret = await LibTodos.add_todo(args)
+      ret.id = 0
+      return ret
+    },
+    updateTodo: async (parent, args, context) => {
+      var ret = await LibTodos.update_todo(args)
+      return ret
+    },
+    deleteTodo: async (parent, args, context) => {
+      var ret = args
+      ret.title = ""
+      var ret = await LibTodos.delete_todo(args)
+      return ret
+    },    
+  }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
